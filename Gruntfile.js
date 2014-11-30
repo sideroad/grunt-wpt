d2b4c8013ac5724365c04c2d71b348b9f64513e3
sideroad <sideroad.jp@gmail.com>
2014-11-30 17:33:35 +0900

    Fixed #10. Fixed #23.

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,6 +70,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-release');
 
   // Whenever the 'test' task is run, first clean the 'tmp' dir, then run this
   // plugin's task(s), then test the result.
