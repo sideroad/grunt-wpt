9da26d8f9ae95590c88642c91f8a6f3e4491f76c
sideroad <sideroad.jp@gmail.com>
2016-09-08 22:11:07 +0900

    Remove unused file

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
-
-app.use(express.static('pages'));
-
-app.listen(port, function () {
-  console.log('Example app listening on ' + port);
-});
